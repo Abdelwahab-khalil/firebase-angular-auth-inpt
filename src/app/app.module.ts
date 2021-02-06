53d8ff46a2b4ec1739e42c9ac91119f52d485e43
ABDELWAHAB KHALIL <khalil.tadla@gmail.com>
2021-02-07 00:20:25 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {AngularFireModule} from '@angular/fire'
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FirebaseService } from './services/firebase.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyB6YwPMxClM3cuK78Q5MUSpHtTqXi1RqMo",
+      authDomain: "fir-angular-auth-a17e4.firebaseapp.com",
+      projectId: "fir-angular-auth-a17e4",
+      storageBucket: "fir-angular-auth-a17e4.appspot.com",
+      messagingSenderId: "372624390874",
+      appId: "1:372624390874:web:15ce440431de5b8f8603e3"
+    }
+    )
   ],
-  providers: [],
+  providers: [FirebaseService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
